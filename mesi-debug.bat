0918c529d7089292dd26bc4608893f6bdb10da7a
nunomourinho <56609351+nunomourinho@users.noreply.github.com>
2023-03-14 21:28:08 +0000

    Relative paths

--- mesi-debug.bat
+++ mesi-debug.bat
@@ -1,4 +1,6 @@
-cd C:\Users\admlocal\Documents\GitHub\forensicVM\main\autopsy-plugin
+SET mypath=%~dp0
+cd %mypath:~0,-1%
+rem cd C:\Users\admlocal\Documents\GitHub\forensicVM\main\autopsy-plugin
 call forensicEnv\scripts\activate.bat
 python forensicVmClient.py %1
 pause
--- mesi-shel.bat
+++ mesi-shel.bat
@@ -1,3 +1,5 @@
-cd C:\Users\admlocal\Documents\GitHub\forensicVM\main\autopsy-plugin
+SET mypath=%~dp0
+cd %mypath:~0,-1%
+rem cd C:\Users\admlocal\Documents\GitHub\forensicVM\main\autopsy-plugin
 call forensicEnv\scripts\activate.bat
 cmd /p
