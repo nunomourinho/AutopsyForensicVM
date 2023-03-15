b06527fc1d4d350cabc29525a00d48c58dd43b7d
nunomourinho <56609351+nunomourinho@users.noreply.github.com>
2023-03-15 23:28:06 +0000

    paramiko ssh key test

---
+++ tests/paramikotest.py
@@ -0,0 +1,28 @@
+import os
+import paramiko
+
+# Generate SSH key pair
+###ssh_key = paramiko.rsakey.RSAKey.generate(2048)
+
+# Save private key to disk
+private_key_path = os.path.expanduser("mykey")
+print(private_key_path)
+###ssh_key.write_private_key_file(private_key_path)
+
+# Save public key to disk
+###public_key_path = f"{private_key_path}.pub"
+###with open(public_key_path, "w") as public_key_file:
+    ###public_key_file.write(f"{ssh_key.get_name()} {ssh_key.get_base64()}")
+
+# Connect to remote host using SSH key authentication
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh.connect('192.168.1.112', username='forensicinvestigator', key_filename=private_key_path, port=22)
+
+# Run a command on the remote host and print the output
+stdin, stdout, stderr = ssh.exec_command('ls -al')
+for line in stdout:
+    print(line.strip())
+
+# Close the SSH connection
+ssh.close()
