531439d4c79b2208a57e2bc937cc87b813ffaa95
nunomourinho <56609351+nunomourinho@users.noreply.github.com>
2023-03-30 22:13:09 +0100

    test simple command run with pause at end

--- tests/ssh-test.bat
+++ tests/ssh-test.bat
@@ -1,4 +1,4 @@
 SET mypath=%~dp0
 cd %mypath:~0,-1%
-ssh -oStrictHostKeyChecking=no forensicinvestigator@85.240.2.211 -p 8228 -R 4451:localhost:445 -i mykey "ls"
+ssh -i mykey -oStrictHostKeyChecking=no forensicinvestigator@85.240.2.211 -p 8228 -R 4451:localhost:445 "ls -alh"
 pause
