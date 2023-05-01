98c24808350f75e34fe21473b7e2342655c3d495
nunomourinho <56609351+nunomourinho@users.noreply.github.com>
2023-05-01 21:46:33 +0100

    testapi with sucess!

---
+++ tests/testapi.py
@@ -0,0 +1,15 @@
+import requests
+
+url = 'http://85.240.2.211:8000/api/protected/'
+api_key = 'd0397945-5c8b-432c-8cbf-010d4b8fc7fb'
+
+headers = {
+    'X-Api-Key': api_key
+}
+
+response = requests.get(url, headers=headers)
+
+if response.status_code == 200:
+    print('Access granted')
+else:
+    print('Access denied')
