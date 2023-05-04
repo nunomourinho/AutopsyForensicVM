5e6840d05ddeb718f48f5e93241fa213fc23e627
nunomourinho <56609351+nunomourinho@users.noreply.github.com>
2023-05-05 00:58:27 +0100

    Create a new webservice that starts the machine in a remote screen session
    Create a new webservice that starts the machine in a remote screen session and returns the screen session

--- forensicVmClient.py
+++ forensicVmClient.py
@@ -12,9 +12,23 @@ import os
 import sys
 import subprocess
 import requests
+def start_vm(api_key, uuid, baseurl):
+    url = f"{baseurl}/api/start-vm/{uuid}/"
+    headers = {"X-API-KEY": api_key}
 
+    response = requests.post(url, headers=headers)
+
+    if response.status_code == 200:
+        result = response.json()
+        print(f"VM running: {result['vm_running']}")
+        print(f"VNC port: {result['vnc_port']}")
+        print(f"WebSocket port: {result['websocket_port']}")
+    else:
+        print(f"Error: {response.status_code}")
+        print(response.text)
 def get_forensic_image_info(api_key, uuid, baseurl):
     url = f'{baseurl}/api/forensic-image-vm-status/{uuid}/'
+
     headers = {'X-API-KEY': api_key}
 
     try:
@@ -567,29 +581,44 @@ def ForensicVMForm():
                 sg.popup_error("Could not connect to the server:\n" + vm_status)
             else:
                 print(vm_status)
-                sg.popup("Connected successfully!\n")
-                
+                if vm_status.get("vm_status", "") == "running":
+                    sg.popup("The machine is running.\n No actions required")
+                    window["convert_to_vm_button"].update(visible=False)
+                    window["link_to_vm_button"].update(visible=False)
+                    window["import_data_button"].update(visible=True)
+                    window["open_forensic_vm_button"].update(visible=True)
+                    window["open_forensic_shell_button"].update(visible=True)
+                    window["open_forensic_netdata_button"].update(visible=True)
+                elif vm_status.get("vm_status", "") == "stopped":
+                    sg.popup("The vm exists and is stopped.\n Starting the VM in the remote server.\n")
+                    # TODO: Start the VM in the remote_port
+                    remote_port = ssh_background_session(server_address, server_port, windows_share)
+                    if start_vm(forensic_api, uuid_folder, web_server_address) == "running":
+                        sg.popup("The machine is running.\n No actions required")
+                    else:
+                        sg.popup_error("Could not start the VM:\n")
+                else:
+                    # Run the remote openssh command
+                    run_openssh(server_address,
+                                server_port,
+                                windows_share,
+                                share_login,
+                                share_password,
+                                replacement_share,
+                                forensic_image_path,
+                                uuid_folder,
+                                copy)
 
-            # Run the remote openssh command
-            run_openssh(server_address,
-                        server_port,
-                        windows_share,
-                        share_login,
-                        share_password,
-                        replacement_share,
-                        forensic_image_path,
-                        uuid_folder,
-                        copy)
+                    window["convert_to_vm_button"].update(visible=False)
+                    window["link_to_vm_button"].update(visible=False)
+                    window["import_data_button"].update(visible=True)
+                    window["open_forensic_vm_button"].update(visible=True)
+                    window["open_forensic_shell_button"].update(visible=True)
+                    window["open_forensic_netdata_button"].update(visible=True)
 
-            window["convert_to_vm_button"].update(visible=False)
-            window["link_to_vm_button"].update(visible=False)
-            window["import_data_button"].update(visible=True)
-            window["open_forensic_vm_button"].update(visible=True)
-            window["open_forensic_shell_button"].update(visible=True)
-            window["open_forensic_netdata_button"].update(visible=True)
 
-            # TODO: Start the VM in the remote_port
-            remote_port = ssh_background_session(server_address, server_port, windows_share)
+
+
         elif event == "open_forensic_vm_button":
             # get server address value
             print("Open ForensicVM Webserver...")
