4a6e99ac0bfc6a7d9c61dd8367b28e3313de64f5
nunomourinho <56609351+nunomourinho@users.noreply.github.com>
2023-05-17 08:05:10 +0100

    Compiled and changed todo

--- compile.bat
+++ compile.bat
@@ -1,6 +1,7 @@
 SET mypath=%~dp0
 cd %mypath:~0,-1%
 call env_forensicEnv\scripts\activate.bat
-pyinstaller --noconfirm --onefile --console --icon "forensicVMClient.ico"  --clean  "forensicVmClient.py"
+rem pyinstaller --noconfirm --onefile --console --icon "forensicVMClient.ico"  --clean -"forensicVmClient.py"
+pyinstaller --hidden-import chardet --hidden-import charset_normalizer.md__mypyc --noconfirm --onefile --icon "forensicVMClient.ico" --clean  "forensicVmClient.py"
 move dist\forensicVmClient.exe forensicVmClient.exe
 pause
